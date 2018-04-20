c2cf1082cb35e1f05dee06b2637e004545779ec6
mrLWachs@gmail.com <lawrence.wachs@28b5cst01.sjsd.lcl>
2018-04-20 14:35:43 -0500

    In class examples

--- src/searchsort/Searcher.java
+++ src/searchsort/Searcher.java
@@ -32,12 +32,12 @@ public class Searcher <T extends Comparable<T>>
      */
     public int linear(T item, T[] array) {
         if (item == null || array == null) return NOT_FOUND;
-        for (int i = 0; i < array.length; i++) {
-            if (item.equals(array[i])) {
-                return i;
+        for (int i = 0; i < array.length; i++) {    // traverse entire list
+            if (item.equals(array[i])) {            // found first occurance
+                return i;                           // stop loop and return
             }
         }
-        return NOT_FOUND;
+        return NOT_FOUND;                           // not found
     }
     
     /**
@@ -51,8 +51,8 @@ public class Searcher <T extends Comparable<T>>
      * @return the first index found at, or a -1 if not found
      */
     public int linear(T item, LinkedList list) {
-        if (item == null || list == null) return NOT_FOUND;
-        return recursiveLinear(item, list, 0);
+        if (item == null || list == null) return NOT_FOUND; // invalids
+        return recursiveLinear(item, list, 0);              // recursive call
     }
     
     /**
@@ -66,7 +66,7 @@ public class Searcher <T extends Comparable<T>>
      * @return the first index found at, or a -1 if not found
      */
     private int recursiveLinear(T item, LinkedList<T> list, int i) {
-        if      (i == list.size())                 return NOT_FOUND;
+        if      (i == list.size())                 return NOT_FOUND;  
         else if (item.compareTo(list.get(i)) == 0) return i;
         else return recursiveLinear(item,list,i+1);
     }
@@ -83,15 +83,15 @@ public class Searcher <T extends Comparable<T>>
      */
     public int binary(T item, T[] array) {
         if (item == null || array == null) return NOT_FOUND;
-        int high = array.length - 1;
-        int low = 0;
-        while (low <= high) {
-            int mid = (high + low) / 2;
+        int high = array.length - 1;    // marker for high end
+        int low = 0;                    // marker for low end
+        while (low <= high) {           // continue while markers not collapsed
+            int mid = (high + low) / 2; // calculate middle point of markers
             if      (array[mid].compareTo(item) == 0) return mid;
             else if (array[mid].compareTo(item)  > 0) high = mid - 1;
             else if (array[mid].compareTo(item)  < 0) low  = mid + 1;
         }
-        return NOT_FOUND;
+        return NOT_FOUND;               // not found
     }
     
     /**
@@ -119,10 +119,9 @@ public class Searcher <T extends Comparable<T>>
      * @param high the highest index to go to
      * @return the first index found at, or a -1 if not found 
      */
-    private int recursiveBinary(T item, LinkedList<T> list, 
-                                int low, int high) {
-        if (low <= high) {
-            int mid = low + (high - low) / 2;
+    private int recursiveBinary(T item,LinkedList<T> list,int low,int high) {
+        if (low <= high) {                      // while markers not collapsed
+            int mid = low + (high - low) / 2;   // calculate middle point 
             if (list.get(mid) == null) return NOT_FOUND;
             if (list.get(mid).compareTo(item) >  0)
                 return recursiveBinary(item,list,low,mid-1);
@@ -131,7 +130,7 @@ public class Searcher <T extends Comparable<T>>
             else 
                 return mid;
         }
-        return NOT_FOUND;
+        return NOT_FOUND;                       // not found
     }
 
 }
