256846e3723bd0b2dd0705abac662c7a72a5011a
mrLWachs@gmail.com <lawrence.wachs@28b5cst01.sjsd.lcl>
2018-04-20 14:20:41 -0500

    In class examples

--- src/searchsort/Sorter.java
+++ src/searchsort/Sorter.java
@@ -2,19 +2,30 @@
 /** required package class namespace */
 package searchsort;
 
+
+/** required imports */
+import collections.LinkedList;
+
 /**
- * Sorter.java - description here...
- *
- * @author Mr. Wachs (login: lawrence.wachs)
- * @since Apr 19, 2018 
+ * Sorter.java - useful methods for sorting arrays and 
+ * 
+ * @author Mr. Wachs
+ * @param <T> the generic object used in this class
+ * @since Feb 9, 2017
  * @instructor Mr. Wachs
  */
-public class Sorter <T extends Comparable<T>>
+public class Sorter <T extends Comparable<T>> 
 {
-    
+       
+    /**
+     * A simple implementation of a bubble sort algorithm
+     * it will sort the array into ascending order
+     * 
+     * @param array the array of generic items to sort
+     */
     public void bubble(T[] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length-1; j++) {
+            for (int j = 0; j < array.length - 1; j++) {
                 T item1 = array[j];
                 T item2 = array[j+1];
                 if (item1.compareTo(item2) > 0) {
@@ -23,8 +34,6 @@ public class Sorter <T extends Comparable<T>>
                 }
             }
         }
-    }
-    
-    
+    }    
     
 }
