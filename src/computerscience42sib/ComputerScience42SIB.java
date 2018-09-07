959770a7d59cb6d46065202abd3c28502c41067c
Mr. Wachs <mrLWachs@gmail.com>
2018-09-07 09:06:41 -0500

    New 2018-2019 School Year

---
+++ src/computerscience42sib/ComputerScience42SIB.java
@@ -0,0 +1,21 @@
+/*
+ *  Mr. Wachs' classes - Java Computer Science learning file ComputerScience42SIB.java
+ *  for project ComputerScience42SIB on 7-Sep-2018 at 9:06:24 AM by lawrence.wachs
+ */
+
+
+/** required package class namespace */
+package computerscience42sib;
+
+
+
+/**
+ * ComputerScience42SIB.java - 
+ *
+ * @author Mr. Wachs 
+ * @since 7-Sep-2018 
+ */
+public class ComputerScience42SIB 
+{
+
+}
