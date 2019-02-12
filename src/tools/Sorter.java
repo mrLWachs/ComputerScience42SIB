51dec96da0c9b22487aee9c1901869bc33ed8bc5
Mr. Wachs <mrLWachs@gmail.com>
2019-02-12 10:53:34 -0600

    New 2018-2019 School Year

--- src/tools/Sorter.java
+++ src/tools/Sorter.java
@@ -14,10 +14,45 @@ import collections.LinkedList;
 public class Sorter <T>
 {
 
+    
     public T[] bubbleSort(T[] array) {
+        
+        //    0     1     2     3     4     5     6
+        // +-----+-----+-----+-----+-----+-----+-----+
+        // |     |     |     |     |     |     |     |
+        // +-----+-----+-----+-----+-----+-----+-----+
+        //    P1    P2
+        
+        // position 1 starts at the begining of list
+        // position 2 is the spot beside it
+        
+        // if position 1 is greater than position 2
+            // swap two items (swap positions)
+        
+        // p1 and p2 advance...
+        
+        // p1 goes to the 2nd last spot in the list
+        
+        // we do this whole operation as many times as 
+        // the list size (worst case scenario)
+        
         return array;
     }   
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     public LinkedList bubbleSort(LinkedList list) {
         return list;
     }   
