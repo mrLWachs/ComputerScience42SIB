df201a77d5c8378a8511cf2eadf46449968f04fa
Mr. Wachs <mrLWachs@gmail.com>
2020-02-06 12:17:10 -0600

    Newest one

--- src/testing/cs42sib/PermanentStorageTest.java
+++ src/testing/cs42sib/PermanentStorageTest.java
@@ -3,8 +3,17 @@
 package testing.cs42sib;
 
 /** required imports */
+import collections.LinkedList;
 import io.Simulator;
 import io.System;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.swing.JFileChooser;
+import java.util.Scanner;
+
 
 
 /**
@@ -22,6 +31,74 @@ public class PermanentStorageTest
     public PermanentStorageTest() {
         Simulator.header("Permanent Storage Test started...");
         
+        Simulator.comment("Error trap examples...");
+        
+        try {
+            double number = Math.PI / 0;            
+            int[] a = { 1, 2, 3 };
+            a[5] = 5;  
+        }
+        catch (ArithmeticException error) {
+            System.out.println(error.toString());
+        }
+        catch (ArrayIndexOutOfBoundsException error) {
+            System.out.println(error.toString());
+        }
+        
+        Simulator.comment("File handling...");
+        
+        String[] poem = {
+          "I should probably sleep",  
+          "And you should too",
+          "But who needs sleep",
+          "When you have nothing to do"
+        };
+        
+        String word = "Sneezel";
+        
+        // set up the error trap
+       
+        try {
+            // creating instances (objects) of class
+            FileWriter  writer  = new FileWriter("data.txt");
+            PrintWriter printer = new PrintWriter(writer);
+            printer.println(word);
+            for (String line : poem) {
+                printer.println(line);
+            }
+            printer.close();            
+        } catch (IOException e) {
+            System.out.println("File error");
+        }
+        
+        // now read the data from the file (open)
+        
+        try {
+            FileReader     reader = new FileReader("data.txt");
+            BufferedReader buffer = new BufferedReader(reader);
+            LinkedList<String> lines = new LinkedList<>();
+            String line = buffer.readLine();
+            while (line != null) {
+                lines.add(line);
+                line = buffer.readLine();
+            }
+            System.out.println(lines.toString());
+            buffer.close();            
+        } catch (IOException e) {
+            System.out.println("File error");
+        }
+        
+        // involve the user        
+        Scanner scanner = new Scanner(java.lang.System.in);
+        String  phrase  = scanner.nextLine();
+        
+        // now we need a file to save it in
+        JFileChooser chooser = new JFileChooser();
+        chooser.showSaveDialog(null);
+        
+        // output the phrase again
+        System.out.println(phrase);
+       
         
         
         Simulator.header("Permanent Storage Test completed!");
