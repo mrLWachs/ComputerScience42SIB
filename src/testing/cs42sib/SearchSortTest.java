2017a5e17996fdd886429b29721333d29e067a9e
Mr. Wachs <mrLWachs@gmail.com>
2020-02-11 12:47:44 -0600

    Newest one

--- src/testing/cs42sib/SearchSortTest.java
+++ src/testing/cs42sib/SearchSortTest.java
@@ -4,6 +4,8 @@ package testing.cs42sib;
 
 /** required imports */
 import io.Simulator;
+import tools.Numbers;
+import tools.Text;
 
 
 /**
@@ -21,7 +23,19 @@ public class SearchSortTest
     public SearchSortTest() {
         Simulator.header("Searching and Sorting Test started...");
         
+        Simulator.comment("For the I.B. exam, only need to know...");
+        Simulator.comment("Linear search and binary search");
+        Simulator.comment("Bubble sort and Selection sort");
                 
+        Simulator.comment("Create random data to search and sort");
+        
+        Numbers numbers = new Numbers();
+        Text    text    = new Text();
+        int[]   array   = numbers.random(-100, 100, 30);
+        
+        
+        System.out.println(text.toString(array));
+        
         
         Simulator.header("Searching and Sorting Test completed!");
     }   
