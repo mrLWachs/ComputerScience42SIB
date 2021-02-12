865f429b9b280ac4d155b49a6f2d8e71d0b69349
Mr. Wachs <mrLWachs@gmail.com>
2021-02-12 12:02:38 -0600

    The assignment solutions finished

--- src/testing/cs42sib/PermanentStorageTest.java
+++ src/testing/cs42sib/PermanentStorageTest.java
@@ -5,6 +5,8 @@ package testing.cs42sib;
 /** required imports */
 import io.Simulator;
 import io.System;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 
 
@@ -40,15 +42,31 @@ public class PermanentStorageTest
         
         Simulator.comment("Create data to use");        
         String[] poem = {
-            "I should probably sleep",  
-            "And you should too",
-            "But who needs sleep",
-            "When you have nothing to do"
+            "There once was a GME stock",
+            "As it aged it sank like a rock",
+            "A hedge fund wanted to profit",
+            "But the stock rose like a rocket"
         };
         
-        String word = "Sneezel";
+        String word = "acolyte";
         
-        
+        try {
+            // Creating instances (objects) of file handling classes
+            String first  = "C:\\Users\\lawrence.wachs\\Desktop\\";
+            String middle = "data";
+            String last   = ".txt";
+            String name   = first + middle + last;
+            // connect the two classes with the file name
+            FileWriter  writer  = new FileWriter(name);
+            PrintWriter printer = new PrintWriter(writer);
+            // Now Write to the file
+            printer.print(word);
+            // Sever (close) the connection to the file
+            printer.close();
+        } 
+        catch (Exception e) {
+            
+        }
         
         
         
