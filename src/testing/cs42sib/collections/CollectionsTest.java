0128d3f9c780284876c81bdc622642a0248d8c6a
Mr. Wachs <mrLWachs@gmail.com>
2021-03-02 13:50:21 -0600

    The assignment solutions finished!

--- src/testing/cs42sib/collections/CollectionsTest.java
+++ src/testing/cs42sib/collections/CollectionsTest.java
@@ -20,30 +20,30 @@ public class CollectionsTest
     public CollectionsTest() {
         Simulator.header("Collections Test started...");
         
-        // Create a node and change (mutate) its data
+        // Create a node and change (mutate) its data..........................
         Simulator.comment("Create a node and change (mutate) its data");
         
         Node<String> a = new Node<>();
         a.data = "Hello";        
         
-        // Create more nodes and the "links" between them
+        // Create more nodes and the "links" between them......................
         Simulator.comment("Create more nodes and the 'links' between them");
         
         Node<String> b = new Node<>("Good-bye");        
         Node<String> c = new Node<>("Exist",a);
         
-        // Use a 'double reference' to mutate data 
+        // Use a 'double reference' to mutate data.............................
         Simulator.comment("Use a 'double reference' to mutate data ");
         
         c.next.data = "Hi";
         
-        // Create a node connected to the other nodes
+        // Create a node connected to the other nodes..........................
         Simulator.comment("Create a node connected to the other nodes");
         
         Node<String> d = new Node<>("Die",b,c);
         d.previous.next.data = "Sup";
         
-        // Output all nodes using overloaded toString method
+        // Output all nodes using overloaded toString method...................
         Simulator.comment("Output all nodes using overloaded toString method");
         
         System.out.println("Node a = " + a.toString());
