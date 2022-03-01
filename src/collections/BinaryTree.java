5183ebd1f27bf4e80d26e43b3f2e6ee219f23361
Mr. Wachs <mrLWachs@gmail.com>
2022-03-01 11:27:28 -0600

    Sample GitHub Project

---
+++ src/collections/BinaryTree.java
@@ -0,0 +1,17 @@
+
+/** Required package class namespace */
+package collections;
+
+ 
+/**
+ * BinaryTree.java - description
+ *
+ * @author Mr. Wachs
+ * @since 28-Feb-2022, 10:16:08 AM
+ */
+public class BinaryTree 
+{
+    
+    
+    
+}
