0ab30818ed3a2abcb163b7aec94024a39bf18bc3
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-02-23 10:45:01 -0600

    Merge origin/Final2023
    Conflicts:
    	src/computerscience30s/ComputerScience30S.java

--- src/testing/cs42sib/PermanentStorageTest.java
+++ src/testing/cs42sib/PermanentStorageTest.java
@@ -6,6 +6,7 @@ package testing.cs42sib;
 import io.Simulator;
 
 
+
 /**
  * PermanentStorageTest.java - tests the concepts learned in this unit
  *
@@ -21,7 +22,45 @@ public class PermanentStorageTest
     public PermanentStorageTest() {
         Simulator.header("Permanent Storage Test started...");
 
+        // Learn about "error traps"...........................................   
         
+        try {
+            // Opening the "try" block (means "try this code"), this is the 
+            // "trap" we set to "try" some code, and if an error occurs (of 
+            // the type we are catching for) we "catch" that error and the  
+            // program keeps running.......................................... 
+            double number = 3 / 0;
+            int[] a = { 1, 2, 3 };
+            a[5] = 0;
+        }
+        catch (ArithmeticException error) {
+            // Open the "catch" block, so if an error occurs in the "try" block
+            // of the type we defined (divide by zero..) the program will 
+            // not shut down, instead it will jump (branch) to this block (the 
+            // catch block).................................................... 
+            System.out.println("Divide by zero -> " + error.toString());
+        }
+        catch (ArrayIndexOutOfBoundsException error) {
+            // You can add multiple "catch" blocks.............................    
+            System.out.println("Array was out of bounds -> " + error.toString());
+        }       
+                
+        // Now some file handling..............................................
+               
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
         
         Simulator.header("Permanent Storage Test completed!");
     }   
