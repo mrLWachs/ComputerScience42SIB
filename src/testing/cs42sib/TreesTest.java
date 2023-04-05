c297c443075b9188d2e2035beecf67b5ade309b1
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-04-05 11:49:16 -0500

    Merge origin/Final2023
    Conflicts:
    	src/computerscience30s/ComputerScience30S.java

--- src/testing/cs42sib/TreesTest.java
+++ src/testing/cs42sib/TreesTest.java
@@ -2,8 +2,10 @@
 /** Required package class namespace */
 package testing.cs42sib;
 
-/** Required imports */
+/** Required API imports */
+import collections.TreeNode;
 import io.Simulator;
+import tools.Numbers;
 
 
 /**
@@ -21,6 +23,51 @@ public class TreesTest
     public TreesTest() {
         Simulator.header("Binary Trees Test started...");
                 
+        // Constants/variables for creating random values......................
+        Simulator.header("Constants/variables for creating random values..."); 
+        
+        final int MIN   = 10;
+        final int MAX   = 99;
+        final int SIZE  = 10;        
+        Numbers numbers = new Numbers();
+        
+        // Creating some TreeNode objects......................................
+        Simulator.comment("Creating some TreeNode objects...");      
+        
+        TreeNode<Integer> node1 = new TreeNode<>(numbers.random(MIN, MAX));
+        TreeNode<Integer> node2 = new TreeNode<>(numbers.random(MIN, MAX));
+        TreeNode<Integer> node3 = new TreeNode<>(numbers.random(MIN, MAX),
+                                                 node1,node2);
+        // Linking the TreeNodes...............................................
+        Simulator.comment("Linking the TreeNodes...");     
+        
+        node1.left  = node2;
+        node1.right = node3;
+        
+        // Testing TreeNode equals..............................................
+        Simulator.comment("Testing TreeNode equals...");    
+        
+        if (node1.equals(node2)) System.out.println("TreeNode1=TreeNode2");
+        if (node1.equals(node3)) System.out.println("TreeNode1=TreeNode3");
+        
+        // Testing TreeNode toString...........................................
+        Simulator.comment("Testing TreeNode toString...");
+        
+        System.out.println(node1.toString());
+        System.out.println(node2.toString());
+        System.out.println(node3.toString());
+        
+        // Creating a Tree object..............................................
+        Simulator.comment("Creating a Tree object...");  
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         Simulator.header("Binary Trees Test completed!");
